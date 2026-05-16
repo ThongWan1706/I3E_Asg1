1048cf99d0194d0b5c2f37337f481de5c91eb423
ThongWan1706 <S10275182@connect.np.edu.sg>
2026-05-16 15:14:13 +0800

    Lvl 1 progress
    Added new assets for the special items

--- Assets/Scripts/SpecialItem.cs
+++ Assets/Scripts/SpecialItem.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 
 public class SpecialItem : MonoBehaviour
 {
-    public int score = 1;
+    public float rotateSpeed = 100f;
+     public int score = 1;
+
+    void Update()
+    {
+        transform.Rotate(0f, rotateSpeed * Time.deltaTime, 0f);
+    }
 }
